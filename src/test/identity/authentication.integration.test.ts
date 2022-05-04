e1b5820012e9bf1f519eaf3f88dcd52b1b00c143
DMH-Pellar <kane.harry.vn@gmail.com>
2022-05-04 13:44:30 +0700

    Clean code on Authentication test

--- src/test/identity/authentication.integration.test.ts
+++ src/test/identity/authentication.integration.test.ts
@@ -1,11 +1,11 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import request from 'supertest'
-import { dbTest, MODELS } from '../init/db'
+import {dbTest, MODELS} from '../init/db'
 import server from '@app/server'
 
 chai.use(chaiAsPromised)
-const { expect, assert } = chai
+const {expect, assert} = chai
 const userData = {
     firstName: 'John',
     lastName: 'Smith',
@@ -16,9 +16,9 @@ const userData = {
     phone: 'phone',
     country: 'country'
 }
-let shareData = { user: {}, token: '', refreshToken: '' }
+let shareData = {user: {}, token: '', refreshToken: ''}
 
-describe('Integration test for module Authentication', () => {
+describe('Authentication', () => {
     before(async () => {
         await dbTest.connect()
     })
@@ -27,47 +27,45 @@ describe('Integration test for module Authentication', () => {
         await dbTest.disconnect()
     })
 
-    context('Authentication', function () {
-        it('Register', async () => {
-            const res = await request(server.app).post('/auth/register').send(userData)
-            expect(res.status).equal(200)
+    it('Register', async () => {
+        const res = await request(server.app).post('/auth/register').send(userData)
+        expect(res.status).equal(200)
 
-            const user = await MODELS.UserModel.findOne({ email: userData.email }).exec()
+        const user = await MODELS.UserModel.findOne({email: userData.email}).exec()
 
-            //Same
-            expect(user?.firstName).equal(userData.firstName)
-            expect(user?.lastName).equal(userData.lastName)
-            expect(user?.nickName).equal(userData.nickName)
-            expect(user?.email).equal(userData.email)
-            expect(user?.phone).equal(userData.phone)
-            expect(user?.country).equal(userData.country)
-            //Different
-            expect(user?.password).not.equal(userData.password)
-            expect(user?.pin).not.equal(userData.pin)
-        }).timeout(10000)
+        //Same
+        expect(user?.firstName).equal(userData.firstName)
+        expect(user?.lastName).equal(userData.lastName)
+        expect(user?.nickName).equal(userData.nickName)
+        expect(user?.email).equal(userData.email)
+        expect(user?.phone).equal(userData.phone)
+        expect(user?.country).equal(userData.country)
+        //Different
+        expect(user?.password).not.equal(userData.password)
+        expect(user?.pin).not.equal(userData.pin)
+    }).timeout(10000)
 
-        it('Login', async () => {
-            const res = await request(server.app).post('/auth/login').send({
-                email: userData.email,
-                password: userData.password
-            })
+    it('Login', async () => {
+        const res = await request(server.app).post('/auth/login').send({
+            email: userData.email,
+            password: userData.password
+        })
 
-            expect(res.status).equal(200)
-            expect(res?.body?.user?.email).equal(userData.email)
-            expect(res?.body?.token).is.a('string')
-            expect(res?.body?.refreshToken).is.a('string')
+        expect(res.status).equal(200)
+        expect(res?.body?.user?.email).equal(userData.email)
+        expect(res?.body?.token).is.a('string')
+        expect(res?.body?.refreshToken).is.a('string')
 
-            shareData.user = res.body.user
-            shareData.token = res.body.token
-            shareData.refreshToken = res.body.refreshToken
-        }).timeout(10000)
+        shareData.user = res.body.user
+        shareData.token = res.body.token
+        shareData.refreshToken = res.body.refreshToken
+    }).timeout(10000)
 
-        it('Logout', async () => {
-            const res = await request(server.app).post('/auth/logout').set('Authorization', `Bearer ${shareData.token}`).send({
-                refreshToken: shareData.refreshToken
-            })
+    it('Logout', async () => {
+        const res = await request(server.app).post('/auth/logout').set('Authorization', `Bearer ${shareData.token}`).send({
+            refreshToken: shareData.refreshToken
+        })
 
-            expect(res.status).equal(200)
-        }).timeout(10000)
-    })
+        expect(res.status).equal(200)
+    }).timeout(10000)
 })
