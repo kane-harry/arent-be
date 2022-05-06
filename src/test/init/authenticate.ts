5a47ea4318a45d47f816bcdd55a80ddcb6db7284
DMH-Pellar <kane.harry.vn@gmail.com>
2022-05-06 15:47:43 +0700

    Handle email verification on Register

--- src/test/init/authenticate.ts
+++ src/test/init/authenticate.ts
@@ -1,9 +1,20 @@
+// @ts-nocheck
 import server from '@app/server'
 import request from 'supertest'
-import {validResponse} from "@app/test/init/db";
+import {MODELS, validResponse} from "@app/test/init/db";
+import {CodeType} from "@modules/verification_code/code.interface";
+import chai from 'chai'
+import {config} from "@config";
+const { expect, assert } = chai
 
 export const register = async (shareData: any, data: object = {}) => {
     const formData = {...userData, ...data}
+
+    if (config.system.registrationRequireEmailVerified) {
+        const code = await getVerificationCode(formData.email)
+        await verifyCode(formData.email, code)
+    }
+
     const res = await request(server.app).post('/auth/register').send(formData)
     validResponse(res.body)
 
@@ -23,4 +34,33 @@ export const userData = {
     pin: '1111',
     phone: 'phone',
     country: 'country'
+}
+
+export const getVerificationCode = async (email: string) => {
+    const res = await request(server.app).post('/verification/code/get').send({
+        codeType: CodeType.EmailRegistration,
+        email: email
+    })
+    expect(res.status).equal(200)
+    validResponse(res.body)
+    const verificationCode = await MODELS.VerificationCode.findOne(
+        {
+            codeType: CodeType.EmailRegistration,
+            email: email
+        },
+        {},
+        { sort: { created_at: -1 } }
+    ).exec()
+    expect(verificationCode?.code).exist
+    return verificationCode?.code
+}
+
+export const verifyCode = async (email: string, code: string) => {
+    const res = await request(server.app).post('/verification/code/verify').send({
+        codeType: CodeType.EmailRegistration,
+        email: email,
+        code: code
+    })
+    expect(res.status).equal(200)
+    validResponse(res.body)
 }
