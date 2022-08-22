781b598a4ffa5f836b35ed2fb4f40de7cade69ff
DMH-Pellar <kane.harry.vn@gmail.com>
2022-08-22 10:18:35 +0700

    Handle owner_key: MASTER

--- src/modules/nft/nft.dto.ts
+++ src/modules/nft/nft.dto.ts
@@ -103,7 +103,11 @@ export class NftRO<T> {
     constructor(nft: any, owner: any, creator: any, collection: any) {
         this.nft = nft
         this.collection = collection
-        this.creator = { key: creator.key, chat_name: creator.chat_name, avatar: creator.avatar }
-        this.owner = { key: owner.key, chat_name: owner.chat_name, avatar: owner.avatar }
+        if (creator) {
+            this.creator = { key: creator.key, chat_name: creator.chat_name, avatar: creator.avatar }
+        }
+        if (owner) {
+            this.owner = { key: owner.key, chat_name: owner.chat_name, avatar: owner.avatar }
+        }
     }
 }
