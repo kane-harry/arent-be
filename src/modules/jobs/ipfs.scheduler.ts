ef316ab87870325d3dd72216a83c65e8efa7fd24
kane-harry <104408527+kane-harry@users.noreply.github.com>
2022-10-26 14:27:04 +0700

    Better log for ipfs job (#163)

--- src/modules/jobs/ipfs.scheduler.ts
+++ src/modules/jobs/ipfs.scheduler.ts
@@ -13,9 +13,14 @@ export default class IpfsScheduler implements IScheduler {
 
     private async handleNftIpfs() {
         const task = cron.schedule('*/2 * * * *', async () => {
+            console.log(`${new Date()} execute task - handleNftIpfs`)
             const nfts = await NftModel.find({ 'image.ipfs_cid': null }, { _id: 0 }).sort({ created: -1 }).limit(10)
             for (let i = 0; i < nfts.length; i++) {
-                await NftService.uploadNftIpfs(nfts[i])
+                try {
+                    await NftService.uploadNftIpfs(nfts[i])
+                } catch (e) {
+                    console.log(`${new Date()} execute task - handleNftIpfs - key: ${nfts[i].key}, error: ${e}`)
+                }
             }
         })
         task.start()
@@ -23,9 +28,14 @@ export default class IpfsScheduler implements IScheduler {
 
     private async handleCollectionIpfs() {
         const task = cron.schedule('*/2 * * * *', async () => {
+            console.log(`${new Date()} execute task - handleCollectionIpfs`)
             const collections = await CollectionModel.find({ 'logo.ipfs_cid': null }, { _id: 0 }).sort({ created: -1 }).limit(10)
             for (let i = 0; i < collections.length; i++) {
-                await CollectionService.uploadCollectionIpfs(collections[i])
+                try {
+                    await CollectionService.uploadCollectionIpfs(collections[i])
+                } catch (e) {
+                    console.log(`${new Date()} execute task - handleCollectionIpfs - key: ${collections[i].key}, error: ${e}`)
+                }
             }
         })
         task.start()
