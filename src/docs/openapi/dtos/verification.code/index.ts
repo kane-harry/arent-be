48ec289700616ec74d4abd7773750250cd5240a4
kane-harry <104408527+kane-harry@users.noreply.github.com>
2022-11-30 09:46:11 +0700

    Building swagger open api (#177)

--- src/docs/openapi/dtos/verification.code/index.ts
+++
@@ -1,6 +0,0 @@
-import createCodeDto from './create.json'
-import verifyCodeDto from './verify.json'
-export default {
-    CreateCodeDto: createCodeDto,
-    VerifyCodeDto: verifyCodeDto
-}
--- src/docs/openapi/responses/index.ts
+++
@@ -1,4 +0,0 @@
-import response422 from './422.json'
-export default {
-    422: response422
-}
