df60cc56e0f054ac7ccf864b6d7acab13d8744e2
DMH-Pellar <kane.harry.vn@gmail.com>
2023-01-29 11:15:22 +0700

    Remove unused module

--- src/modules/category/category.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsOptional } from 'class-validator'
-
-export class CategoryDto {
-    @IsNotEmpty()
-    public name: string
-
-    @IsOptional()
-    public description: string
-}
--- src/modules/category/category.interface.ts
+++
@@ -1,7 +0,0 @@
-import IBaseModel from '@interfaces/base.model.interface'
-
-export interface ICategory extends IBaseModel {
-    nav_key: string
-    name: string
-    description: string
-}
